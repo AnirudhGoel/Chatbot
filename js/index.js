05a7ead094648ff1e20cc54c9c6866dd01ffe633
Anirudh Goel <Anirudhgoel.delhi@gmail.com>
2016-09-13 23:37:05 +0530

    Major UI Improvements

--- js/index.js
+++ js/index.js
@@ -8,15 +8,24 @@ function reply(event) {
 	console.log("Hello");
 	if (checkEnter(event) == "true") {
 		var query = $(".text").text();
-		$(".chat-body").append("<div class='query'>" + query + "</div>");
-		$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
-		console.log(query);
 		$(".text").text("");
+		$(".chat-body").append("<div class='query'>" + query + "</div>");
+		$(".query").fadeIn(1000);
+		$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
+
+		$(".chat-body").append("<div class='load'><img src='img/loading_dots.gif' class='loader'></div>");
+		$(".load").fadeIn(1000);
+		console.log($(".load"));
+		$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
+		
 		$.get("../Chatbot/chatbot.php", {q: query}, function(data) {
 			data = JSON.parse(data);
 			var reply = data["result"];
-			$(".chat-body").append("<div class='reply'>" + reply + "</div>");
-			$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
+			$(".load").fadeOut(1000, function() {
+				$(".chat-body").append("<div class='reply'>" + reply + "</div>");
+				$(".reply").fadeIn(1000);
+				$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
+			});
 		})
 	}
 }
