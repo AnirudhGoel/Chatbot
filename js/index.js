c18a92543520bb8f4425c7ca1c3cbb8c6ada7fad
Anirudh Goel <Anirudhgoel.delhi@gmail.com>
2016-09-13 23:49:17 +0530

    Loader added + FadeIn effect added

--- js/index.js
+++ js/index.js
@@ -10,20 +10,19 @@ function reply(event) {
 		var query = $(".text").text();
 		$(".text").text("");
 		$(".chat-body").append("<div class='query'>" + query + "</div>");
-		$(".query").fadeIn(1000);
+		$(".query").last().fadeIn(200);
 		$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
 
 		$(".chat-body").append("<div class='load'><img src='img/loading_dots.gif' class='loader'></div>");
-		$(".load").fadeIn(1000);
-		console.log($(".load"));
+		$(".load").last().fadeIn(1500);
 		$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
 		
 		$.get("../Chatbot/chatbot.php", {q: query}, function(data) {
 			data = JSON.parse(data);
 			var reply = data["result"];
-			$(".load").fadeOut(1000, function() {
+			$(".load").last().fadeOut(200, function() {
 				$(".chat-body").append("<div class='reply'>" + reply + "</div>");
-				$(".reply").fadeIn(1000);
+				$(".reply").last().fadeIn(200);
 				$(".chat-body").animate({ scrollTop: $(".chat-body")[0].scrollHeight}, 1000);
 			});
 		})
