6878d3bbac5dcd3769a3743fa3dff32eaa1aa658
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2017-04-25 19:19:16 +0530

    added trial.php to try heroku key api

---
+++ trial.php
@@ -0,0 +1,15 @@
+<?php
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/vnd.heroku+json"));
+curl_setopt($ch, CURLOPT_URL, "https://api.heroku.com/apps/edu-chatbot/config-vars");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+curl_close($ch);
+if ($curl_errno == 0) {
+    echo($data);
+} else {
+	echo "error";
+}
+
+?>
